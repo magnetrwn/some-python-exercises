26219061b08f67758be4c66130b0a932df95da7e
magnetrwn <94263372+magnetrwn@users.noreply.github.com>
2023-02-23 18:02:21 +0100

    Boosted performance, fixed queue filling up

--- leibniz/leibniz-mp.py
+++ leibniz/leibniz-mp.py
@@ -1,33 +1,41 @@
 # Leibniz approach to Pi, with multiprocessing (every process calculates
-# two pieces of the leibniz series, then skips cpu_count() pieces for the 
+# two pieces of the leibniz series, then skips cpu_count() pieces for the
 # other processes to calculate them)
 
-# WARNING: could fill RAM
+# WARNING: once the queue is filled, calculation slows down drastically,
+#          based on the single-process loop of endless_get_pi()!
+#          Try to balance the two default variables for best speed on your
+#          platform.
 
 from multiprocessing import Process, Queue, cpu_count
 
-def get_leibniz_piece_worker(start_from, q):
+_QUEUE_PUT_GROUPS = 2048
+_QUEUE_SIZE = 262144
+
+def get_leibniz_piece_worker(start_from, q, group_by=_QUEUE_PUT_GROUPS):
   d = 1+start_from*4
   while True:
-    pi = 1/d
-    d += 2
-    pi -= 1/d
-    d += 2+(cpu_count()-1)*4
+    pi = 0
+    for _ in range(group_by):
+      pi += 1/d
+      d += 2
+      pi -= 1/d
+      d += 2+(cpu_count()-1)*4
     q.put(pi)
 
 def get_fixed_charlist(flo):
   return list(str('{0:.15f}'.format(flo)))
 
-def endless_get_pi(rate):
+def endless_get_pi(rate, queue_size=_QUEUE_SIZE, group_by=_QUEUE_PUT_GROUPS):
   pi, showtext, shown = 0, 0, 0
   pi_text = get_fixed_charlist(0.0)
   pi_text_old = get_fixed_charlist(0.0)
-  
-  q = Queue()
-  
+
+  q = Queue(queue_size)
+
   for p in range(cpu_count()):
-    Process(target=get_leibniz_piece_worker, args=(p, q)).start()
-  
+    Process(target=get_leibniz_piece_worker, args=(p, q, group_by)).start()
+
   while True:
     pi += q.get()
     showtext += 1
@@ -40,9 +48,9 @@ def endless_get_pi(rate):
           pi_text[idx] = '\x1B[42m'+pi_text[idx]+'\x1B[0m'
         else:
           break
-      if shown == 64:
+      if shown == 32:
         shown = 0
         pi_text_old = get_fixed_charlist(4*pi)
       print('[ leibniz-mp.py | pi:', ''.join(pi_text), ']', end='\r')
 
-endless_get_pi(512)
+endless_get_pi(2048)
